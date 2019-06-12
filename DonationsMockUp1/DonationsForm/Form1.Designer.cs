3524aeb87d2dbcc91a23150d6430a7968d9057e9
dtrowbri7669 <donaldjaketrowbridge@yahoo.com>
2019-06-12 14:33:50 -0700

    Add files via upload

---
+++ DonationsMockUp1/DonationsForm/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace DonationsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAsin = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudQty = new System.Windows.Forms.NumericUpDown();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPallet = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCase = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.cmbProfile = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtProduct = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ASIN";
+            // 
+            // txtAsin
+            // 
+            this.txtAsin.Location = new System.Drawing.Point(79, 27);
+            this.txtAsin.Name = "txtAsin";
+            this.txtAsin.Size = new System.Drawing.Size(141, 20);
+            this.txtAsin.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(79, 200);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(413, 20);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "QTY";
+            // 
+            // nudQty
+            // 
+            this.nudQty.Location = new System.Drawing.Point(79, 63);
+            this.nudQty.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudQty.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudQty.Name = "nudQty";
+            this.nudQty.Size = new System.Drawing.Size(141, 20);
+            this.nudQty.TabIndex = 2;
+            this.nudQty.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(79, 140);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(141, 20);
+            this.txtWeight.TabIndex = 4;
+            this.txtWeight.TextChanged += new System.EventHandler(this.txtChange);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Weight/Unit";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(79, 103);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(141, 20);
+            this.txtValue.TabIndex = 3;
+            this.txtValue.TextChanged += new System.EventHandler(this.txtChange);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Price/Unit";
+            // 
+            // txtPallet
+            // 
+            this.txtPallet.Location = new System.Drawing.Point(351, 27);
+            this.txtPallet.Name = "txtPallet";
+            this.txtPallet.Size = new System.Drawing.Size(141, 20);
+            this.txtPallet.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Pallet";
+            // 
+            // txtCase
+            // 
+            this.txtCase.Location = new System.Drawing.Point(351, 62);
+            this.txtCase.Name = "txtCase";
+            this.txtCase.Size = new System.Drawing.Size(141, 20);
+            this.txtCase.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(276, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Case";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(351, 100);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(141, 20);
+            this.txtLogin.TabIndex = 9;
+            this.txtLogin.TextChanged += new System.EventHandler(this.txtChange);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(274, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Login";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(351, 226);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(145, 226);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // cmbProfile
+            // 
+            this.cmbProfile.FormattingEnabled = true;
+            this.cmbProfile.Location = new System.Drawing.Point(79, 173);
+            this.cmbProfile.MaxDropDownItems = 20;
+            this.cmbProfile.Name = "cmbProfile";
+            this.cmbProfile.Size = new System.Drawing.Size(413, 21);
+            this.cmbProfile.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 176);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Profile";
+            // 
+            // txtProduct
+            // 
+            this.txtProduct.Location = new System.Drawing.Point(351, 140);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(141, 20);
+            this.txtProduct.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(274, 143);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Product Type";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 261);
+            this.Controls.Add(this.txtProduct);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmbProfile);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtCase);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPallet);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudQty);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAsin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Artorias";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.nudQty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAsin;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudQty;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPallet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCase;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox cmbProfile;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtProduct;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ DonationsMockUp1/DonationsForm/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace DonationsForm
+{
+    public partial class Form1 : Form
+    {
+        private FileStream file;
+        private FileStream HazFile;
+        private StreamReader ReadingRainbow;
+        private StreamWriter stream;
+        private DateTime date = DateTime.Now;
+        private string filename;
+        private string hazFileName = "HazType.txt";
+        private int size = 96;
+        private string[] profile; 
+        private double[] disposal;
+        private bool[] pounds; 
+        private const string Delim = ",";
+        private int fileCount = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            profile = new string[size];
+            disposal = new double[size];
+            pounds = new bool[size];
+            OpenHazFile();
+            ReadHaz();
+            filename = "Donations_" + date.Month + "_" + date.Day + "_" + date.Year + ".csv";
+            if (!File.Exists(filename))
+            {
+                CreateFile();
+            }
+            else
+            {
+                 OpenFile();
+            }
+        }
+        //Clears configs
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void clear()
+        {
+            txtAsin.Clear();
+            txtCase.Clear();
+            txtPallet.Clear();
+            txtTitle.Clear();
+            txtValue.Clear();
+            txtWeight.Clear();
+            txtProduct.Clear();
+            nudQty.Value = 1;
+        }
+
+        //Validates logins
+        private bool ValidateLogin()
+        {
+            string pattern = @"^[a-zA-Z]+$";
+            Regex regex = new Regex(pattern);
+            bool match = regex.IsMatch(txtLogin.Text);
+            if (!regex.IsMatch(txtLogin.Text))
+            {
+                InvalidInput(txtLogin);
+            }
+            return match;
+        }
+        //Validates price
+        private bool ValidateValue()
+        {
+            string pattern = @"^\d+[.]\d{1,2}$|^\d+$";
+            Regex regex = new Regex(pattern);
+            bool match = regex.IsMatch(txtValue.Text);
+            if (!match)
+            {
+                InvalidInput(txtValue);
+            }
+            return match;
+        }
+        //Validates weight of object
+        private bool ValidateWeight()
+        {
+            string pattern = @"^\d{1,3}\.\d{1,2}$|^\d{1,3}$";
+            Regex regex = new Regex(pattern);
+            bool match = regex.IsMatch(txtWeight.Text);
+            if (!match)
+            {
+                InvalidInput(txtWeight);
+            }
+            return match;
+        }
+        //checks if remaining fields are null
+        private bool IsFieldsNull()
+        {
+            bool asin = txtAsin.TextLength == 0;
+            bool title = txtTitle.TextLength == 0;
+            bool pallet = txtPallet.TextLength == 0;
+            bool lcase = txtCase.TextLength == 0;
+            bool ptype = txtProduct.TextLength == 0;
+            bool valid = true;
+            if (asin || title || pallet || lcase || ptype)
+                valid = false;
+            return valid;
+        }
+
+        //changes text back after input has been correctedl
+        private void txtChange(object sender, EventArgs e)
+        {
+            TextBox current = sender as TextBox;
+            current.ForeColor = Color.Black;
+            current.BackColor = Color.White;
+        }
+        //Changes text when incorrect
+        private void InvalidInput(TextBox t)
+        {
+            t.ForeColor = Color.Red;
+            t.BackColor = Color.LightSalmon;
+        }
+
+
+        //checks submit criteria
+        private bool CheckInput()
+        {
+            bool value =ValidateValue();
+            bool weight = ValidateWeight();
+            bool login = ValidateLogin();
+            bool fields = IsFieldsNull();
+            bool valid = true;
+            if (!value || !weight || !login || !fields)
+               valid = false;
+            return valid;
+        }
+        //submits input
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //runs validation
+            if (CheckInput())
+                writeToFile();
+        }
+
+        //Create File
+        private void CreateFile()
+        {
+            try
+            {
+                file = new FileStream(filename, FileMode.Create, FileAccess.Write);
+                stream = new StreamWriter(file);
+                string x = "Asin" + Delim + "Title" + Delim + "QTY" + Delim + "Weight/Unit" +
+                        Delim + "Price/Unit" + Delim + "Total Weight" + Delim + "Total Value" +
+                        Delim + "Date" + Delim + "Pallet" + Delim + "Case" + Delim + "Product Type" +
+                        Delim + "Login" + Delim + "Profile Type" + Delim + "Disposal Price";
+                stream.WriteLine(x);
+                stream.Flush();
+            }catch(IOException)
+            {
+                filename = "Donations_" + date.Month + "_" + date.Day + "_" + date.Year + "(" + fileCount.ToString() + ").csv";
+                fileCount++;
+                CreateFile();
+            }
+        }
+        
+        //open file
+        private void OpenFile()
+        {
+
+            try
+            {
+                file = new FileStream(filename, FileMode.Append, FileAccess.Write);
+                stream = new StreamWriter(file);
+            }
+            catch(IOException)
+            {
+                filename = "Donations_" + date.Month + "_" + date.Day + "_" + date.Year + "(" + fileCount.ToString() + ").csv";
+                fileCount++;
+                CreateFile();
+            }
+        }
+
+        //gets current input, puts it into a string
+        private void writeToFile()
+        {
+            double price;
+            double weight;
+            try
+            {
+                price = Convert.ToDouble(txtValue.Text) * Convert.ToDouble(nudQty.Value);
+                weight = Convert.ToDouble(txtWeight.Text) * Convert.ToDouble(nudQty.Value);
+                double disposalPrice = 0;
+                int sel = cmbProfile.SelectedIndex;
+                if(pounds[sel])
+                {
+                    disposalPrice = disposal[sel] * weight;
+                }
+                else
+                {
+                    disposalPrice = disposal[sel] * Convert.ToDouble(nudQty.Value);
+                }
+                string title = txtTitle.Text;
+                title = title.Replace(Delim, "");
+                string x = "";
+                x += txtAsin.Text + Delim + title + Delim + nudQty.Value + Delim;
+                x += txtWeight.Text + Delim + "$" + txtValue.Text + Delim + weight.ToString() + Delim;
+                x += price.ToString() + Delim;
+                x += date.Month + "/" + date.Day + "/" + date.Year + "/" + " " + date.Hour + ":" + date.Minute + ":" + date.Second;
+                x += Delim + txtPallet.Text + Delim;
+                x += txtCase.Text + Delim + txtProduct.Text + Delim + txtLogin.Text + Delim + profile[sel].ToString();
+                x += Delim + "$" + disposalPrice.ToString();
+                stream.WriteLine(x);
+                stream.Flush();
+                clear();
+            }
+            catch(InvalidCastException)
+            {
+                MessageBox.Show("The values in the Price or Weight field are invalid. Please correct and try again.");
+            }  
+        }
+
+
+        //Close File
+        private void CloseFile()
+        {
+            stream.Flush();
+            stream.Close();
+            file.Close();
+        }
+
+        private void OpenHazFile()
+        {
+            try
+            {
+                HazFile = new FileStream(hazFileName, FileMode.Open, FileAccess.Read);
+                ReadingRainbow = new StreamReader(HazFile);
+            }
+            catch(FileNotFoundException)
+            {
+                MessageBox.Show("The file \"HazType.txt\" is missing or renamed.");
+            }
+        }
+
+        private void ReadHaz()
+        {
+            int count = 0;
+            while(!ReadingRainbow.EndOfStream)
+            {
+                string line = ReadingRainbow.ReadLine();
+                string[] input = new string[3];
+                input = line.Split(',');
+                profile[count] = input[0];
+                disposal[count] = Convert.ToDouble(input[1]);
+                if (input[2].Equals("lb"))
+                {
+                    pounds[count] = true;
+                }
+                else
+                {
+                    pounds[count] = false;
+                }
+                count++;
+            }
+            setCmb();
+        }
+        
+        private void setCmb()
+        {
+            cmbProfile.DataSource = profile;
+        }
+
+        //closes file when form closes
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseFile();
+        }
+    }
+}
